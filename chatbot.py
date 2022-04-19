8ef6289cdbdc5beb8a12632958cf0b8c824f7fe0
zhouchenh <37189091+zhouchenh@users.noreply.github.com>
2022-04-19 23:08:39 +0800

    Fix the mysql requirement in the requirements.txt

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 import queue
 
 import flask
+import mysql.connector
 import telegram
 import telegram.ext
 
@@ -8,21 +9,18 @@ import environment
 import handlers
 import webhook
 
-import mysql.connector
-
 
 class Chatbot:
-
     def __init__(self, env=None):
         if env is None:
             env = environment.Env()
         self.env = env
         self.bot = telegram.Bot(token=env.telegram_access_token)
         self.db = mysql.connector.connect(
-            host =env.db_host,
-            user =env.db_user,
-            passwd =env.db_password,
-            database = env.db_name
+            host=env.db_host,
+            user=env.db_user,
+            passwd=env.db_password,
+            database=env.db_name
         )
         if env.production:
             self.dispatcher = telegram.ext.Dispatcher(bot=self.bot, update_queue=queue.Queue())
@@ -35,8 +33,6 @@ class Chatbot:
             self.dispatcher.add_handler(handler[1])
         if env.production:
             webhook.WebHook(self.env, self.app, self.bot, self.dispatcher)
-        
-        
 
     def start_polling(self):
         self.updater.start_polling()
