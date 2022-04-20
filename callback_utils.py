b0d7dc2de47fc9c99549809dd09ffb88a3af4f46
zhouchenh <37189091+zhouchenh@users.noreply.github.com>
2022-04-20 23:18:04 +0800

    Change the argument type for callback_chat function

--- callback_utils.py
+++ callback_utils.py
@@ -1,6 +1,10 @@
 import json
+from typing import Optional, TYPE_CHECKING
 
 from db_table.callback_data import store, store_chat
+
+if TYPE_CHECKING:
+    from telegram import Chat
 
 
 def callback(call: str, data, db):
@@ -10,8 +14,10 @@ def callback(call: str, data, db):
     }), db))
 
 
-def callback_chat(chat_id: int, call: str, data, db):
-    return str(store_chat(chat_id, json.dumps({
-        'call': call,
-        'data': data
-    }), db))
+def callback_chat(chat: Optional['Chat'], call: str, data, db):
+    if chat is not None:
+        return str(store_chat(chat.id, json.dumps({
+            'call': call,
+            'data': data
+        }), db))
+    return None
