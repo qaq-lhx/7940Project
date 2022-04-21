844df80b6bb0fa66da64a8ec997d0f4904e237f7
zhouchenh <37189091+zhouchenh@users.noreply.github.com>
2022-04-21 19:50:48 +0800

    Add pagination for search command

--- handler/search.py
+++ handler/search.py
@@ -1,43 +1,157 @@
+import json
+import math
+from typing import List, Tuple, Optional
+
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, CallbackContext
 
-from callback_utils import callback
+from callback_utils import callback, callback_chat
+from db_table.callback_data import store
 from db_table.movie_info import search_movie_in_db
+from db_table.rating import get_movie_average_ratings
 from handler import GetChatbot
 
 
-def build_search_results(keywords, results, db):
-    reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton(
-            '{} ({})'.format(result[1], result[2]),
-            callback_data=callback('search_callback', {
-                'action': 'show_movie_info',
-                'selected_id': result[0],
-                'search_keywords': keywords
-            }, db)
-        )] for result in results])
+def search_with_ratings(keywords: List[str], excluded_ids: List[int], limit: int, db) -> \
+        List[Tuple[int, str, str, float]]:
+    results = search_movie_in_db(keywords, excluded_ids, limit, db)
+    ratings = get_movie_average_ratings([result[0] for result in results], db)
+    return [result + (ratings[result[0]],) for result in results]
+
+
+def build_pagination_page_numbers(total: int, page: int, page_limit: int) -> \
+        Tuple[Optional[int], List[Optional[int]], Optional[int]]:
+    max_page = math.ceil(total / page_limit)
+    pagination_page_numbers = [page]
+    prev_page = None,
+    next_page = None,
+    if page > 1:
+        prev_page = page - 1
+        pagination_page_numbers.insert(0, prev_page)
+    if page < max_page:
+        next_page = page + 1
+        pagination_page_numbers.append(next_page)
+    if page == 1 and 3 <= max_page:
+        pagination_page_numbers.append(3)
+    if page == max_page and max_page - 2 >= 1:
+        pagination_page_numbers.insert(0, max_page - 2)
+    if pagination_page_numbers[0] > 1:
+        if pagination_page_numbers[0] > 2:
+            pagination_page_numbers.insert(0, None)
+        pagination_page_numbers.insert(0, 1)
+    if pagination_page_numbers[-1] < max_page:
+        if pagination_page_numbers[-1] < max_page - 1:
+            pagination_page_numbers.append(None)
+        pagination_page_numbers.append(max_page)
+    return prev_page, pagination_page_numbers, next_page
+
+
+def build_pagination_button(results_id: int, current_page: int, page: Optional[int], page_limit: int, db):
+    if page is None:
+        return InlineKeyboardButton('\u2026', callback_data='-1')
+    if page == current_page:
+        return InlineKeyboardButton('<{}>'.format(page), callback_data='-1')
+    return InlineKeyboardButton(str(page), callback_data=callback('search_callback', {
+        'action': 'show_search_results',
+        'search_results_id': results_id,
+        'page': page,
+        'page_limit': page_limit
+    }, db))
+
+
+def build_search_results(results_id: Optional[int], results: List[Tuple[int, str, str, float]], page: int,
+                         page_limit: int, update_markup_only: bool, db):
     if len(results) < 1:
-        return 'I can\'t find any movie for you.', None
-    if len(results) > 1:
-        message = 'Here are the movies I found:'
+        if update_markup_only:
+            return None, None
+        else:
+            return 'I can\'t find any movie for you.', None
+    if results_id is None:
+        results_id = store(json.dumps(results), db)
+    total = len(results)
+    need_pagination = total > page_limit
+    pagination_buttons = None
+    if need_pagination:
+        prev_page, pagination_page_numbers, next_page = build_pagination_page_numbers(total, page, page_limit)
+        results_to_show = results[(page - 1) * page_limit:page * page_limit]
+        pagination_buttons = [build_pagination_button(results_id, page, page_number, page_limit, db) for page_number in
+                              pagination_page_numbers]
+        if prev_page is not None:
+            pagination_buttons.insert(0, InlineKeyboardButton('\u1438', callback_data=callback('search_callback', {
+                'action': 'show_search_results',
+                'search_results_id': results_id,
+                'page': prev_page,
+                'page_limit': page_limit
+            }, db)))
+        if next_page is not None:
+            pagination_buttons.append(InlineKeyboardButton('\u1433', callback_data=callback('search_callback', {
+                'action': 'show_search_results',
+                'search_results_id': results_id,
+                'page': next_page,
+                'page_limit': page_limit
+            }, db)))
     else:
-        message = 'Here is the movie I found:'
-    return message, reply_markup
+        results_to_show = results
+    buttons_to_show = [[InlineKeyboardButton(
+        '{} ({}), \u2606:{:0.1f}'.format(result[1], result[2], result[3]),
+        callback_data=callback('search_callback', {
+            'action': 'show_movie_info',
+            'selected_id': result[0],
+            'search_results_id': results_id,
+            'page': page,
+            'page_limit': page_limit
+        }, db)
+    )] for result in results_to_show]
+    if need_pagination:
+        print(pagination_buttons)
+        # buttons_to_show.append(pagination_buttons)
+        buttons_to_show.append([InlineKeyboardButton('test', callback_data='-1')])
+    if update_markup_only:
+        message = None
+    else:
+        if len(results) > 1:
+            message = 'Here are the movies I found:'
+        else:
+            message = 'Here is the movie I found:'
+    return message, InlineKeyboardMarkup(buttons_to_show)
+
+
+def new_search(query, query_data, update: Update, context: CallbackContext):
+    if 'text' in query_data:
+        keywords = query_data['text'].split(' ')
+    else:
+        keywords = query_data['search_keywords']
+    limit = query_data['search_limit']
+    page = query_data['page']
+    page_limit = query_data['page_limit']
+    message, reply_markup = build_search_results(
+        None,
+        search_with_ratings(keywords, [], limit, chatbot().db),
+        page,
+        page_limit,
+        False,
+        chatbot().db
+    )
+    if message is not None:
+        if reply_markup is None:
+            update.message.reply_text(message)
+        else:
+            update.message.reply_text(message, reply_markup=reply_markup)
 
 
 def search_command(update: Update, context: CallbackContext):
-
+    query_data = {
+        'action': 'new_search',
+        'search_limit': chatbot().env.search_limit,
+        'page': 1,
+        'page_limit': chatbot().env.page_limit
+    }
     if len(context.args) < 1:
-        update.message.reply_text('Usage: /search <keyword>')
-        return
-    keywords = context.args
-    message, reply_markup = build_search_results(keywords, search_movie_in_db(keywords, chatbot().db), chatbot().db)
-    if reply_markup is None:
-        update.message.reply_text(message)
-        return
+        update.message.reply_text('What do you want to search for?')
+        callback_chat(update.effective_chat, 'search_callback', query_data, chatbot().db)
     else:
-        update.message.reply_text(message, reply_markup=reply_markup)
-        return 'search_callback'
+        query_data['search_keywords'] = context.args
+        new_search(None, query_data, update, context)
 
 
 chatbot = GetChatbot()
--- handler/search_callback.py
+++ handler/search_callback.py
@@ -1,25 +1,41 @@
+import json
+
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from telegram.ext import CallbackContext
 
 from callback_utils import callback
-from db_table.movie_info import get_movie_in_db, search_movie_in_db
+from db_table.callback_data import fetch
+from db_table.movie_info import get_movie_in_db
 from handler import GetChatbot
-from handler.search import build_search_results
+from handler.search import build_search_results, new_search
 
 
-def show_movie_info(query, query_data):
+def show_movie_info(query, query_data, update: Update, context: CallbackContext):
     movie_id = query_data['selected_id']
-    keywords = query_data['search_keywords']
+    results_id = query_data['search_results_id']
+    page = query_data['page']
+    page_limit = query_data['page_limit']
     movie = get_movie_in_db(movie_id, chatbot().db)
     reply_markup = InlineKeyboardMarkup([
         [InlineKeyboardButton('\u1438', callback_data=callback('search_callback', {
-            'action': 'search_again',
-            'search_keywords': keywords
+            'action': 'show_search_results',
+            'search_results_id': results_id,
+            'page': page,
+            'page_limit': page_limit,
+            'update_text': True
         }, chatbot().db)),
-        #click to evaluate a movie
-        InlineKeyboardButton('evaluate',callback_data=callback('evaluate',{'movie_id':movie_id,'movie':movie[1]}, chatbot().db)),
-        #click to view comment of a movie
-        InlineKeyboardButton('comment',callback_data=callback('view',{'movie_id':movie_id}, chatbot().db))],
+         # click to evaluate a movie
+         InlineKeyboardButton('evaluate', callback_data=callback(
+             'evaluate',
+             {'movie_id': movie_id, 'movie': movie[1]},
+             chatbot().db
+         )),
+         # click to view comment of a movie
+         InlineKeyboardButton('comment', callback_data=callback(
+             'view',
+             {'movie_id': movie_id},
+             chatbot().db
+         ))],
     ])
     if movie is None:
         message = 'Oops! I\'m sorry. I can\'t tell you more about the movie.'
@@ -32,28 +48,50 @@ def show_movie_info(query, query_data):
         )
     query.edit_message_text(message)
     query.edit_message_reply_markup(reply_markup)
-    return 'search_callback'
 
 
-def search_again(query, query_data):
-    keywords = query_data['search_keywords']
-    message, reply_markup = build_search_results(keywords, search_movie_in_db(keywords, chatbot().db), chatbot().db)
-    if reply_markup is None:
-        query.edit_message_text(message)
-        return
+def show_search_results(query, query_data, update: Update, context: CallbackContext):
+    results_id = query_data['search_results_id']
+    page = query_data['page']
+    page_limit = query_data['page_limit']
+    update_markup_only = 'update_text' not in query_data or not query_data['update_text']
+    raw_search_results = fetch(results_id, chatbot().db)
+    if raw_search_results is None:
+        return query.edit_message_text('Oops! I\'m sorry. I can\'t let you proceed.')
+    search_results = json.loads(raw_search_results)
+    message, reply_markup = build_search_results(
+        results_id,
+        search_results,
+        page,
+        page_limit,
+        update_markup_only,
+        chatbot().db
+    )
+    if message is None:
+        if reply_markup is not None:
+            query.edit_message_reply_markup(reply_markup)
     else:
-        query.edit_message_text(message)
-        query.edit_message_reply_markup(reply_markup)
-        return 'search_callback'
+        if reply_markup is None:
+            query.edit_message_text(message)
+        else:
+            query.edit_message_text(message)
+            query.edit_message_reply_markup(reply_markup)
 
 
 def search_callback(query: CallbackQuery, query_data, update: Update, context: CallbackContext):
-    return actions[query_data['action']](query, query_data)
+    if 'instance' in query_data and 'text' in query_data and 'data' in query_data:
+        data = query_data['data']
+        data['instance'] = query_data['instance']
+        data['text'] = query_data['text']
+    else:
+        data = query_data
+    actions[data['action']](query, data, update, context)
 
 
 actions = {
+    'new_search': new_search,
     'show_movie_info': show_movie_info,
-    'search_again': search_again
+    'show_search_results': show_search_results
 }
 chatbot = GetChatbot()
 Callback = chatbot, search_callback
