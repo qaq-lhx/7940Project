442c2e95cc34818c612cb8a2ace41cfad20b9669
904782056@qq.com <904782056@qq.com>
2022-04-21 17:54:50 +0800

    solve conflict

--- callbacks.py
+++ callbacks.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-from handler import search_callback, button_callback, help_callback,rating,rating_callback,label_callback
-=======
-
-from handler import search_callback, button_callback, help_callback,rating,rating_callback, recommend_callback
->>>>>>> 6e458f2bc7c7b0dca056a5c77753212e329248e4
+from handler import search_callback, button_callback, help_callback,rating,rating_callback,label_callback,recommend_callback
 
 
 Callbacks = {
@@ -12,10 +7,7 @@ Callbacks = {
     'help_callback': help_callback.Callback,
     'evaluate':rating.Callback,
     'rating_callback':rating_callback.Callback,
-<<<<<<< HEAD
     'label_callback':label_callback.Callback,
-=======
     'recommend_callback': recommend_callback.Callback,
 
->>>>>>> 6e458f2bc7c7b0dca056a5c77753212e329248e4
 }
