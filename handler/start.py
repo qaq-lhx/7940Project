06dbfb4aa9646a24b7d2a01e3a80d1d8f254fe3e
21470138 <21470138@life.hkbu.edu.hk>
2022-04-22 01:22:24 +0800

    add start function

--- handler/start.py
+++ handler/start.py
@@ -1,16 +1,16 @@
 #need to modify
 import logging
 
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, CallbackContext
+
+from callback_utils import callback
 
 from handler import GetChatbot
 
 
-def hello_command(update: Update, context: CallbackContext) -> None:
-    logging.info(context.args)
-    update.message.reply_text('Good day, ' + ' '.join(context.args) + '!')
-
+def starting(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Start to find a movie you like!\nYou can follow our guide to Find/Rate the movie\nSeacrhing movie:/seacrh <keyword>\nRecommend movie:/recommend <keyword>')
 
 chatbot = GetChatbot()
-Handler = chatbot, CommandHandler('start', hello_command)
+Handler = chatbot, CommandHandler('start', starting)
