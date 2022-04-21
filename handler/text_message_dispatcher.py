26ce40e9dbf8fe74e7df89f1359190d170280318
zhouchenh <37189091+zhouchenh@users.noreply.github.com>
2022-04-22 06:34:11 +0800

    Fix a bug in text_message_dispatcher

--- handler/text_message_dispatcher.py
+++ handler/text_message_dispatcher.py
@@ -11,13 +11,14 @@ from handler.default import default_reply
 
 def handle_text_message(update: Update, context: CallbackContext):
     chat = update.effective_chat
-    if chat is not None:
+    message = update.message
+    if chat is not None and message is not None:
         instance, raw_data = fetch_chat(chat.id, chatbot().db)
         if instance is not None and raw_data is not None:
             query_data = json.loads(raw_data)
             data = {
                 'instance': instance,
-                'text': update.message.text,
+                'text': message.text,
                 'data': query_data['data']
             }
             Callbacks[query_data['call']][1](update.callback_query, data, update, context)
