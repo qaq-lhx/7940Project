7ca78b97856de64a309d74b42322ca7dfe761451
904782056@qq.com <904782056@qq.com>
2022-04-22 01:02:53 +0800

    some comment problem

--- handler/write_comment_callback.py
+++ handler/write_comment_callback.py
@@ -8,7 +8,7 @@ from db_table import feedback
 def write_comment_callback(query: CallbackQuery, query_data, update: Update, context: CallbackContext):
     comment = query_data['text']
     movie_id = query_data['data']
-    feedback_id = query.message.message_id
+    feedback_id = update.message.message_id
     feedback.add_comment(feedback_id,movie_id,comment,chatbot().db)
     context.bot.send_message(chat_id=update.effective_chat.id, text='Thanks for your sharing!\nYou can use command \"/search <keyword>\" or simply use command \"/search\" to find another movie.')
     undo_callback_chat_all(update.effective_chat, chatbot().db)
