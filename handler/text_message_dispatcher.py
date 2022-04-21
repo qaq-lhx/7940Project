c460b9e07e09209c9caa6e70b1283d4c80da847e
zhouchenh <37189091+zhouchenh@users.noreply.github.com>
2022-04-22 03:19:25 +0800

    Fix a bug in text message dispatcher

--- handler/text_message_dispatcher.py
+++ handler/text_message_dispatcher.py
@@ -6,11 +6,7 @@ from telegram.ext import MessageHandler, Filters, CallbackContext
 from callbacks import Callbacks
 from db_table.callback_data import fetch_chat
 from handler import GetChatbot
-from words import get_some_random_words
-
-
-def default_reply(update: Update, context: CallbackContext):
-    update.message.reply_text(get_some_random_words('default') + '\n\n' + get_some_random_words('start_tip'))
+from handler.default import default_reply
 
 
 def handle_text_message(update: Update, context: CallbackContext):
@@ -25,6 +21,7 @@ def handle_text_message(update: Update, context: CallbackContext):
                 'data': query_data['data']
             }
             Callbacks[query_data['call']][1](update.callback_query, data, update, context)
+            return
     default_reply(update, context)
 
 
