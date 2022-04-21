b7c0eafc8784067d33a4348663e08bb8f30b6595
zhouchenh <37189091+zhouchenh@users.noreply.github.com>
2022-04-22 02:56:50 +0800

    Remove unused code in help callback

--- handler/help_callback.py
+++ handler/help_callback.py
@@ -8,7 +8,6 @@ from handler import GetChatbot
 def help_callback(query: CallbackQuery, query_data, update: Update, context: CallbackContext):
     reply_message = 'Let me see... "{}"? I don\'t quite understand.'.format(query_data['text'])
     context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
-    # undo_callback_chat(query_data['instance'], chatbot().db)
     undo_callback_chat_all(update.effective_chat, chatbot().db)
 
 
