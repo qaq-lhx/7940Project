2a336af1788760ef12b56eb820b9bea9cc04ef7f
zhouchenh <37189091+zhouchenh@users.noreply.github.com>
2022-04-22 07:40:14 +0800

    Add buttons in the start menu

--- handler/start.py
+++ handler/start.py
@@ -1,16 +1,35 @@
-#need to modify
-import logging
-
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CommandHandler, CallbackContext
 
 from callback_utils import callback
-
 from handler import GetChatbot
+from handler.search import prepare_data_for_new_search
 
 
 def starting(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Start to find a movie you like!\nYou can follow our guide to Find/Rate the movie!')
+    reply_markup = InlineKeyboardMarkup([[
+        InlineKeyboardButton('Search', callback_data=callback(
+            'search_callback',
+            prepare_data_for_new_search(),
+            chatbot().db
+        )),
+        InlineKeyboardButton('Recommend', callback_data=callback(
+            'recommend_callback',
+            {'action': 'new_recommend'},
+            chatbot().db
+        ))
+    ]])
+    update.message.reply_text(
+        'Start to find a movie you like!\n' +
+        '\n' +
+        'You can follow our guide to Find/Rate the movie!\n' +
+        '\n' +
+        'Search: Search for a movie with /search.\n' +
+        '\n' +
+        'Recommend: Get some recommendations with /recommend.',
+        reply_markup=reply_markup
+    )
+
 
 chatbot = GetChatbot()
 Handler = chatbot, CommandHandler('start', starting)
