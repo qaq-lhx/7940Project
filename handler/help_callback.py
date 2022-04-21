bba019bb7d71c19cf1889ff58c9b3a5c067079da
zhouchenh <37189091+zhouchenh@users.noreply.github.com>
2022-04-22 02:56:24 +0800

    Update the message to reply in help callback

--- handler/help_callback.py
+++ handler/help_callback.py
@@ -6,7 +6,7 @@ from handler import GetChatbot
 
 
 def help_callback(query: CallbackQuery, query_data, update: Update, context: CallbackContext):
-    reply_message = 'Let me see... "{}"? I\'ll give it a try.'.format(query_data['text'])
+    reply_message = 'Let me see... "{}"? I don\'t quite understand.'.format(query_data['text'])
     context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
     # undo_callback_chat(query_data['instance'], chatbot().db)
     undo_callback_chat_all(update.effective_chat, chatbot().db)
