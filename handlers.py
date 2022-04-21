b49044e270fb68922c13f2f36c2e147a94b11a8d
zhouchenh <37189091+zhouchenh@users.noreply.github.com>
2022-04-22 03:12:58 +0800

    Deregister the unused hello command

--- handlers.py
+++ handlers.py
@@ -1,12 +1,11 @@
-from handler import text_message_dispatcher, callback_query_dispatcher, start, help, hello,  \
-    add_label,  search, button, recommend
+from handler import text_message_dispatcher, callback_query_dispatcher, start, help, add_label, search, button, \
+    recommend
 
 Handlers = [
     text_message_dispatcher.Handler,
     callback_query_dispatcher.Handler,
     start.Handler,
     help.Handler,
-    hello.Handler,
     add_label.Handler,
     search.Handler,
     button.Handler,
