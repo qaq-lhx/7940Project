067631c316156ac5ad41c786e39f170558d34cbb
zhouchenh <37189091+zhouchenh@users.noreply.github.com>
2022-04-22 01:38:30 +0800

    Close cursors in label

--- db_table/label.py
+++ db_table/label.py
@@ -3,6 +3,7 @@ def get_labels_from_db(movie_id,db):
     cursorObject = db.cursor()
     cursorObject.execute( "SELECT label FROM Label WHERE movieID=%s ORDER BY frequency LIMIT 13", (movie_id,))
     result = cursorObject.fetchall()
+    cursorObject.close()
     return result
 
 
@@ -10,9 +11,11 @@ def add_label_to_db(movie_id,label,db):
     cursorObject = db.cursor()
     cursorObject.execute( "SELECT frequency FROM Label WHERE movieID=%s and label=%s ", (movie_id,label))
     result = cursorObject.fetchall()
-    if len(result)==0:    
-        cursorObject.execute( """INSERT INTO Label (movieID,label,frequency) VALUES ( %s , %s , %s )""",(movie_id,label,1))
+    if len(result) == 0:
+        cursorObject.execute("""INSERT INTO Label (movieID,label,frequency) VALUES ( %s , %s , %s )""",
+                             (movie_id, label, 1))
     else:
         frequency = int(result[0][0])+1
         cursorObject.execute( """UPDATE Label SET frequency=%s WHERE movieID=%s and label=%s""",(frequency,movie_id,label))
+    cursorObject.close()
     db.commit()
